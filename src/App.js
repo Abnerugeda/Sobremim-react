e1e5c75d49171ed97f00e77763fae5a867509591
abnerugeda <devabnerugeda@gmail.com>
2023-05-27 19:08:44 -0300

    Inicio Projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./screens/Inicio";
+import Sobremim from "./screens/Sobremim";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>}/>
+        <Route path="/sobremim" element={<Sobremim/>}/>
+        <Route path="*" element={<div>Página não encontrada</div>}/>
+      </Routes>
+    </BrowserRouter>
+  )  
 }
 
 export default App;
